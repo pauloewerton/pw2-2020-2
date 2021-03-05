c0cb5b59ed2b2fb9e5dc0f66f81ba355d09191b7
Paulo Ewerton <pauloewerton@icloud.com>
2021-03-05 18:49:15 -0300

    Aula 06

--- aula05/src/App.js
+++ aula05/src/App.js
@@ -1,38 +1,64 @@
 import React from 'react';
+import ContatoForm from "./components/ContatoForm";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import './App.css';
 
 function Agenda() {
+  let [contatos, setContatos] = React.useState([
+    {"nome": "Joao", "telefone": "83 88998899"},
+    {"nome": "Maria", "telefone": "84 87798899"},
+    {"nome": "Pedro", "telefone": "85 99998899"}
+  ]);
+
+  let [novoContato, setNovoContato] = React.useState({ nome: '', telefone: ''});
+
+  const onContatoChange = (name, value) => {
+    let contatoCopia = Object.assign({}, novoContato);
+    contatoCopia[name] = value;
+
+    setNovoContato(contatoCopia);
+  };
+
+  const salvarContato = (evt) => {
+    evt.preventDefault();
+    let contatosCopia = [...contatos];
+    contatosCopia.push(novoContato);
+    setContatos(contatosCopia);
+  };
+
   return (
     <React.Fragment>
-      <Navegacao />
-      <Main />
+      <Navegacao onContatoChange={onContatoChange} onContatoSubmit={salvarContato} />
+      <Main contatos={contatos} />
     </React.Fragment>
   );
 }
 
-function Navegacao() {
-  const [n, setN] = React.useState(1);
-  setInterval(() => {
-    setN(n + 1);
-  }, 1000);
-
+function Navegacao(props) {
   return (
     <header>
       <nav className="navbar navbar-light bg-light">
         <span className="navbar-brand mb-0 h1 nav-titulo">Agenda</span>
-        <span>{n}</span>
-        <a href="/novocontato" className="btn btn-primary" role="button">Criar Contato</a>
+          <button type="button" className="btn btn-primary" data-toggle="modal"
+                  data-target="#contatoModal">
+            Criar contato
+          </button>
+          <React.Fragment>
+            <ContatoForm onContatoChange={props.onContatoChange} salvarContato={props.onContatoSubmit} />
+          </React.Fragment>
       </nav>
     </header>
   );
 }
 
-function Main() {
+function Main(props) {
   return (
     <section>
       <div className="container main-section">
         <Rastreador caminho="Contatos" />
-        <Contatos />
+        <Contatos contatos={props.contatos} />
       </div>
     </section>
   );
@@ -50,23 +76,18 @@ function Rastreador(props) {
 
 function Contato(props) {
   return (
-    <div className="card shadow-sm bg-white rounded">
-      <div className="card-body contato">
-        <h4 className="card-title contato-titulo">{props.nome}</h4>
-        <h3 className="card-title">{props.telefone}</h3>
+    <div className="card shadow-sm bg-white rounded contato">
+      <div className="card-body">
+        <h3 className="card-title contato-titulo">{props.nome}</h3>
+        <h4 className="card-title">{props.telefone}</h4>
       </div>
     </div>
   );
 }
 
-function Contatos() {
-  const contatos = [
-    {"nome": "Joao", "telefone": "83 88998899"},
-    {"nome": "Maria", "telefone": "84 87798899"},
-    {"nome": "Pedro", "telefone": "85 99998899"}
-  ];
+function Contatos(props) {
 
-  const listaContatos = contatos.map((contato, idx) => {
+  let listaContatos = props.contatos.map((contato, idx) => {
     return <Contato nome={contato.nome} telefone={contato.telefone} key={idx} />;
   });
 
@@ -77,14 +98,10 @@ function Contatos() {
   );
 }
 
+
 function App() {
   return (
     <Agenda />
-    //<div className="App">
-    //  <h1>Olá, PW2!</h1>
-    //</div>
-    //React.createElement('div', { className: 'App' },
-    //  React.createElement('h1', null, 'Olá, PW2!'));
   );
 }
 
---
+++ aula05/src/components/ContatoForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+function ContatoForm(props) {
+  const handleChange = (evt) => {
+    let { name, value } = evt.target;
+    props.onContatoChange(name, value);
+  };
+
+  return (
+    <div className="modal fade" id="contatoModal" tabIndex="-1" aria-labelledby="contatoModalLabel"
+         aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="contatoModalLabel">Criar contato</h5>
+            <button type="button" className="close" data-dismiss="modal" aria-label="Fechar">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="contato-nome" className="col-form-label">Nome:</label>
+                <input type="text" className="form-control" id="contato-nome" name="nome"
+                       onChange={handleChange} />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="contato-telefone" className="col-form-label">Telefone:</label>
+                <input type="text" className="form-control" id="contato-telefone" name="telefone"
+                       onChange={handleChange} />
+              </div>
+            </form>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-dismiss="modal">
+              Cancelar
+            </button>
+            <button type="button" className="btn btn-primary" data-dismiss="modal"
+                    onClick={props.salvarContato}>
+              Criar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContatoForm;
